ed813ea27724eaaa4d9954699b0636456c935d7c
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2023-07-29 08:09:52 +0600

    Создание базового контроллера со всеми основными Action

---
+++ LibraryApp.API/Controllers/DataValuesController.cs
@@ -0,0 +1,12 @@
+﻿using LibraryApp.API.Controllers.Base;
+using LibraryApp.DAL.Entities;
+using LibraryApp.Interfaces.Base.Repositories;
+
+namespace LibraryApp.API.Controllers
+{
+    public class DataValuesController : EntityController<DataValue>
+    {
+        public DataValuesController(IRepository<DataValue> repository) : base(repository) { }
+
+    }
+}
